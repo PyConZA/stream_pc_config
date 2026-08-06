53c5c736db1af2ea4f57529d779aaa7111c40463
Adam Piskorski <adam@piskorski.me>
2026-08-06 15:03:55 +0000

    feat: Add homebrew and OBS

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Nix and other configuration files for PyConZA Stream PCs
 
 ## Setup new mac
-1. Install nix with `curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh`
-2. Clone this config repo when in the home directory with `git clone https://github.com/PyConZA/stream_pc_config.git`
+1. Make sure that your user name is **streamer**
+2. Install nix with `curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh`
+3. Clone this config repo when in the home directory with `git clone https://github.com/PyConZA/stream_pc_config.git`
   1. Allow developer tools to be installed in order to use git.
-3. Move pre-existing managed config files that will cause nix-darwin to abort installation:
+4. Move pre-existing managed config files that will cause nix-darwin to abort installation:
   1. `sudo mv /etc/bashrc /etc/bashrc.before-nix-darwin && sudo mv /etc/zshrc /etc/zshrc.before-nix-darwin`
-4. Install nix-darwin: `sudo nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/stream_pc_config#stream-pc`
+5. Install nix-darwin: `sudo nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/stream_pc_config#stream-pc`
--- flake.nix
+++ flake.nix
@@ -5,9 +5,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, ... }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -31,13 +40,63 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      nixpkgs.config.allowUnfree = true;
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "obs"
+        ]
+      };
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#steam-pc
     darwinConfigurations."stream-pc" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "streamer";
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+
+            # Optional: Declarative Homebrew tap trust entries.
+            #
+            # Note: The trust entries are _not_ removed if you remove them from those lists!
+            # Use the `brew untrust` command to remove a trust entry.
+            trust = {
+              formulae = [ ];
+              casks = [ ];
+              commands = [ ];
+              taps = [ ];
+            };
+          };
+        }
+        # Optional: Align homebrew taps config with nix-homebrew
+        ({config, ...}: {
+          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+        })
+      ];
     };
   };
 }
